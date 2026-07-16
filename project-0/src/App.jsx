a6a30fdbbfe13b5a25e8eaf25c0972ed22dae838
Er Rahul Singh <errahul@Ers-MacBook-Air.local>
2026-07-16 12:32:26 +0530

    16th July 2nd Part

--- project-0/src/App.jsx
+++ project-0/src/App.jsx
@@ -3,12 +3,48 @@ import './App.css'
 import { StudentComponent } from './components/StudentComponent'
 
 function App() {
- 
+   
+  let studentInformation = [
+    {
+      name: "Transformer1",
+      age: 1006,
+      course: "Transform"
+    },
+    {
+      name: "Transformer2",
+      age: 1007,
+      course: "Btech"
+    },
+    {
+      name: "Transformer3",
+      age: 1008,
+      course: "Btech Civil"
+    },
+    {name: "Transformer4",
+      age: 1009,
+      course: "Btech Mechanical"},
+    {name: "Transformer5",
+      age: 1010,
+      course: "Btech Ai "},
+    {name: "Transformer6",
+      age: 1011,
+      course: "Btech Printing "},
+      {name: "Transformer7",
+      age: 1011,
+      course: "Btech Printing "}
+  ]
 
   return (
     <>
-     <StudentComponent name="Kunal" age={78} course="Nahi Btaunga"/>
-     <StudentComponent name="Kunali" age={79} course="Kya pata"> Kuch bhi likhdo</StudentComponent>
+      
+       {studentInformation.map(transformer => (
+        <StudentComponent 
+          name={transformer.name}
+          age = {transformer.age}
+          course = {transformer.course}
+          
+          />
+       ))}
     </>
    
   )
